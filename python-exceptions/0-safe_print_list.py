b438d579b479babe5c45ca3211b48038018b24c7
WagnerMushayija <mushayijawagner@gmail.com>
2024-07-03 07:42:52 -0700

    printing using try and except

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    printed_nums = 0
+    try:
+        for i in range (x):
+            print(my_list[i], end='')
+            printed_nums += 1
+    except:
+        pass
+    return printed_nums
