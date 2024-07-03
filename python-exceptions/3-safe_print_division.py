e8fea17452cccf45589707f782982ae286447aec
WagnerMushayija <mushayijawagner@gmail.com>
2024-07-03 11:23:44 -0700

    own trial

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/isr/bin/python3
+def safe_print_division(a, b):
+    try:
+        pass
+    except(ValueError, TypeError):
+        continue
+    finally:
+        result = a / b
+        print("{:d} / {:d} = {}".format(a, b, result))
