0716226c7365d934951e7538e9d382f6501d68f8
WagnerMushayija <mushayijawagner@gmail.com>
2024-07-03 09:05:02 -0700

    removed error

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,10 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     printed_nums = 0
     try:
-        for i in range (x):
+        for i in range(x):
             print(my_list[i], end='')
             printed_nums += 1
-    except:
+    except IndexError:
         pass
     print()
     return printed_nums
