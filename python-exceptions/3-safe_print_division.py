74445f4d1a6e004137999b8cca4af76018042e17
WagnerMushayija <mushayijawagner@gmail.com>
2024-07-05 11:50:11 -0700

    number 3 done

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        pass
-    except(ValueError, TypeError):
-        continue
-    finally:
         result = a / b
-        print("{:d} / {:d} = {}".format(a, b, result))
+    except:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return (result)
