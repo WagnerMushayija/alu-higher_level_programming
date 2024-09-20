ff9247b0d4c96f48d4067c86ab2b1dc37d54f710
WagnerMushayija <mushayijawagner@gmail.com>
2024-09-20 23:41:23 +0200

    consoling the log

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+if (argv == 1){
+    console.log('Argument found');
+}
+else if (argv > 1){
+    console.log('Arguments found');
+}
+else {
+    console.log('No argument');
+}
