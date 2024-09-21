a9bd41070d09a24dc22ac462a0bad8900e25d847
WagnerMushayija <mushayijawagner@gmail.com>
2024-09-21 12:53:43 +0200

    consoling the log

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,20 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-let count = 0;
-argv.forEach(() => {
-    count++;
-});
-
-if (count <= 2) {
-    console.log('No argument');
+if (argv.length <= 2) {
+  console.log('No argument');
 } else {
-    let index = 0;
-    argv.forEach((arg) => {
-        if (index >= 2) {
-            console.log(arg[2]);
-        }
-        index++;
-    });
+  console.log(argv[2]);
 }
 
