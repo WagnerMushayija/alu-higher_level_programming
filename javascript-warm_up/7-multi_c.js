aef60562937aac27e70159cb624c143bc312671a
WagnerMushayija <mushayijawagner@gmail.com>
2024-09-23 13:06:15 +0200

    consoling the log

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,9 +4,9 @@ const { argv } = require('node:process');
 const argCount = argv[2];
 
 if (argCount === undefined || (isNaN(argCount))) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < argCount; i++) {
-        console.log(`C is fun`);
+  for (let i = 0; i < argCount; i++) {
+    console.log(`C is fun`);
     }
 }
