f590630d3e47db03692eadf08be1b69da2c8f6c6
WagnerMushayija <mushayijawagner@gmail.com>
2024-09-23 10:52:23 +0200

    consoling the log

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -3,9 +3,9 @@ const { argv } = require('node:process');
 const argCount = argv.length - 2;
 
 if (argCount === 0) {
-    console.log('undefined is undefined');
+  console.log('undefined is undefined');
 } else if (argCount === 1) {
-    console.log(`${argv[2]} is undefined`);
+  console.log(`${argv[2]} is undefined`);
 }else {
-    console.log(`${argv[2]} is ${argv[3]}`);
+  console.log(`${argv[2]} is ${argv[3]}`);
 }
