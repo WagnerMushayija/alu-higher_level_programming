e07b7bd18d462d461ba019369341dbd50369aa47
WagnerMushayija <mushayijawagner@gmail.com>
2024-09-24 15:59:36 +0200

    consoling the log

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -9,7 +9,7 @@ switch (argsCount) {
 
   default:
     for (let i = 2; i < argsCount; i++) {
-       numbers.push(process.argv[i]);
+      numbers.push(process.argv[i]);
     }
     numbers.sort((a, b) => b - a);
     console.log(numbers[1]);
